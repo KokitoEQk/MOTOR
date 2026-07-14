08d10e9c5addb3ea4a133a0d8d390a0b7d5db69b
KokitoEQk <festoproekt1@abv.bg>
2026-07-14 11:32:52 +0300

    Finale

--- Proj/gui/controller.py
+++ Proj/gui/controller.py
@@ -1,8 +1,9 @@
 from gui.view import Window 
-
+from PyQt5.QtWidgets import QPushButton
 import os
 import tkinter as tk
 from PIL import Image, ImageTk, ImageSequence
+from PyQt5.QtCore import Qt
 
 
 class controller:
@@ -15,8 +16,12 @@ class controller:
         self.speed = 0
         
         # Start Button
-        self.view.btn_start.clicked.connect(self.motor_power)
+        self.view.btn_start.setCheckable(True)
         
+        self.view.btn_start.clicked.connect(lambda:self.motor_power(self.view.btn_start.isChecked()))
+       
+        
+        self.view.btn_start.clicked.connect(lambda:self.change_color_btn(button=self.view.btn_start, color="#00FF00"))
         #Reset motor button 
         self.view.btn_reset.clicked.connect(self.controller_reset)
         
@@ -25,17 +30,17 @@ class controller:
         #Position traker enable button
         self.view.btn_position_traker.clicked.connect(self.motor_position_traker)
         #Revolution set
-
+        
+        
 
         
         self.view.movieScr.setMovie(self.view.GIf_movie)
+        self.view.movieScr.setScaledContents(True)
+        self.view.movieScr.setFixedSize(800,400)
+        self.view.movieScr.setAlignment(Qt.AlignCenter)
         self.view.GIf_movie.start()
         self.view.GIf_movie.stop()
-
-    def motor_power(self):
-       
-        self.view.lbl_test.setText("STARTING...")
-        self.view.GIf_movie.start()
+        
     def controller_reset(self):
         self.view.GIf_movie.stop()
         self.view.lbl_test.setText("Controller reset")
@@ -47,8 +52,14 @@ class controller:
         self.degree = self.view.le_R.text() 
         self.rev = int(self.degree)/360
         self.view.lbl_test.setText(f"DEGREES:{self.rev}")
+        
         self.speed = self.view.le_S.text()
-        self.view.lbl_test.setText("Motor is moving")
+        if(int(self.speed) < 30):
+
+            self.view.lbl_test.setText("Motor is moving")
+        else:
+            self.view.lbl_test.setText("Set speed under 30")
+            
         return self.rev, self.speed
        
     def motor_revolution(self):
@@ -56,6 +67,39 @@ class controller:
         
     def motor_speed(self):
         self.view.lbl_test.setText("Set speed")
+
+    def change_color_btn(self, button:QPushButton,color):
+        button.setStyleSheet(f"""
+            background-color: {color};
+            font-weight: bold;
+            border-color: rgb(0,30,100);
+                             """)        
+    
+    def motor_power(self, is_checked):
+        if(is_checked):
+            self.view.btn_start.setText("MOTOR: ON")
+            self.view.btn_start.setStyleSheet(f"""
+            background-color: #00FF00;
+            font-weight: bold;
+            border-style: solid;
+            border-width: 3px;
+            
+            
+                             """)       
+        else:
+            self.view.btn_start.setText("MOTOR: OFF")
+            self.view.btn_start.setStyleSheet(f"""
+            background-color: #FF0000;
+            font-weight: bold;
+            border-style: solid;
+            border-width: 3px;
+            
+                             """)       
+    
+
+
+
+
 
        
 
